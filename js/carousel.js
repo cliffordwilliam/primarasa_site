ac2867c59a58b9d256b15ef05246f1e4899e52bb
Clifford William <ccliffordwilliam@gmail.com>
2023-06-17 01:41:43 +0800

    Remade the whole thing with my new framework

--- js/carousel.js
+++ js/carousel.js
@@ -1,29 +1,68 @@
-var carouselContainer = document.querySelector(".carousel-container");
-var prevButton = document.querySelector(".prev-button");
-var nextButton = document.querySelector(".next-button");
-
-var slides = Array.from(carouselContainer.querySelectorAll(".carousel-slide"));
-var totalSlides = slides.length;
-var currentSlide = 0;
-
-function showSlide() {
-    slides.forEach(function(slide) {
-    slide.style.display = "none";
+function initializeCarousel(carouselId, isAuto = false) {
+    const carouselContainer = document.querySelector(`#${carouselId} .carousel-container`);
+    const carouselItems = document.querySelectorAll(`#${carouselId} .carousel-item`);
+    const nextButton = document.querySelector(`#${carouselId} .next-button`);
+    const prevButton = document.querySelector(`#${carouselId} .prev-button`);
+  
+    let oldIndex = 0;
+    let currentIndex = 0;
+    const itemCount = carouselItems.length;
+    let isTransitioning = false;
+    let autoplayInterval;
+    let isAutoplay = isAuto;
+  
+    nextButton.addEventListener("click", function() {
+      if (isTransitioning) {
+        return;
+      }
+      oldIndex = currentIndex;
+      currentIndex = (currentIndex + 1) % itemCount;
+      updateCarousel("slide-left");
     });
-    slides[currentSlide].style.display = "block";
-}
-
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide();
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide();
-}
-
-prevButton.addEventListener("click", prevSlide);
-nextButton.addEventListener("click", nextSlide);
-
-showSlide();
+  
+    prevButton.addEventListener("click", function() {
+      if (isTransitioning) {
+        return;
+      }
+      oldIndex = currentIndex;
+      currentIndex = (currentIndex - 1 + itemCount) % itemCount;
+      updateCarousel("slide-right");
+    });
+  
+    function updateCarousel(transitionClass) {
+      stopAutoplay();
+      isTransitioning = true;
+      carouselContainer.classList.toggle(transitionClass);
+      carouselItems[oldIndex].classList.remove("lead");
+      carouselItems[currentIndex].classList.add("lead", "show");
+    }
+  
+    carouselContainer.addEventListener("transitionend", function() {
+      isTransitioning = false;
+      carouselContainer.classList.remove("slide-left", "slide-right");
+      carouselItems[oldIndex].classList.remove("show");
+      if (isAutoplay) {
+        startAutoplay();
+      }
+    });
+  
+    function startAutoplay() {
+      autoplayInterval = setInterval(() => {
+        oldIndex = currentIndex;
+        currentIndex = (currentIndex + 1) % itemCount;
+        updateCarousel("slide-left");
+      }, 3000);
+    }
+  
+    function stopAutoplay() {
+      clearInterval(autoplayInterval);
+    }
+  
+    if (isAutoplay) {
+      startAutoplay();
+    }
+  }
+  
+  initializeCarousel("carousel1", true);
+  initializeCarousel("carousel2", true);
+  
